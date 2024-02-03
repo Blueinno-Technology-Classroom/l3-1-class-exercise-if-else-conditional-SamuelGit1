29cab156606cdecb0557e929eb63aec5136e8670
SamuelGit1 <144694613+SamuelGit1@users.noreply.github.com>
2024-02-03 14:22:14 +0800

    submit

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ Q2:
 a: y > 0
 b: z != 0
 c: y > z
-d: z > 0
+d: z >= 0
 e: (x - y < z - y and x > 0 and z > 0) or (x - y > z - y and x < 0 and z < 0)
 f: z % 2 == 1
 g: x % 2 == 0
 h: y % z == 0
-i: 10 > y > 0
-j: -x == z
+i: 10 > y >= 0
+j: (x > 0 and z < 0) or (z > 0 and x < 0)
 k: (x % 2 == 0 and y % 2 == 1) or (y % 2 == 0 and x % 2 == 1) 
 '''
 
